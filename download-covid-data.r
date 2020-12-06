a2d6ac3f81569c51d6873d9d5688beabf8ac3497
luca-terrazzan <luca.terraz@gmail.com>
2020-12-06 17:28:16 +0100

    feat: read and write latest covid data for Italy

---
+++ download-covid-data.r
@@ -0,0 +1,7 @@
+if (!file.exists('data/global-latest-data.csv')) {
+  raw_csv_from_github = read.csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv')
+  write.csv(raw_csv_from_github, 'data/global-latest-data.csv')
+}
+
+covid_data = read.csv('data/global-latest-data.csv')
+covid_data
