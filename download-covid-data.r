4dce7b5871cd188507a1f549352c59597507a2e7
luca-terrazzan <luca.terraz@gmail.com>
2020-12-07 12:40:03 +0100

    fix: fixed positive tests percentages

--- download-covid-data.r
+++ download-covid-data.r
@@ -1,3 +1,5 @@
+library(useful)
+
 if (!file.exists('data/global-latest-data.csv')) {
   raw_csv_from_github = read.csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv')
   write.csv(raw_csv_from_github, 'data/global-latest-data.csv')
@@ -5,12 +7,18 @@ if (!file.exists('data/global-latest-data.csv')) {
 
 covid_data = read.csv('data/global-latest-data.csv')
 
-covid_data['positivi/tamponi'] = covid_data['totale_positivi'] / covid_data['tamponi']
+# Enrich data with new informations
+covid_data = shift.column(data=covid_data, columns="tamponi", newNames='nuovi_tamponi', up=FALSE)
+covid_data['nuovi_tamponi'] = covid_data['tamponi'] - covid_data['nuovi_tamponi']
+covid_data['positivi/tamponi'] = covid_data['nuovi_positivi'] / covid_data['nuovi_tamponi']
+
 write.csv(covid_data, 'data/global-latest-data-with-proportions.csv')
 
-dates = as.Date(covid_data['data'][,1])
-percentages = covid_data['positivi/tamponi'][,1]
-positives = covid_data['totale_positivi'][,1]
+# Save new dataset
 
-plot(data.frame(dates, percentages), '', 'positives / tests', type='l', col='red')
-plot(data.frame(dates, positives), '', 'total positives', type='l', col='yellow')
+dates = as.Date(covid_data['data'][,1])
+
+plot(data.frame(dates, covid_data['positivi/tamponi'][,1]), '', 'positives / tests', type='l', col='red')
+plot(data.frame(dates, covid_data['totale_positivi'][,1]), '', 'total positives', type='l', col='yellow')
+plot(data.frame(dates, covid_data['nuovi_positivi'][,1]), '', 'daily positives', type='l', col='black')
+plot(data.frame(dates, covid_data['tamponi']), '', 'tests', type='l', col='black')
