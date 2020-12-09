2992ab7fa8cee2201ec8e470347c8f8aefc6cd59
luca-terrazzan <luca.terraz@gmail.com>
2020-12-09 11:55:48 +0100

    chore: formatting

--- download-covid-data.r
+++ download-covid-data.r
@@ -1,10 +1,10 @@
 library(useful)
 
+# Download latest data from github if needed
 if (!file.exists('data/global-latest-data.csv')) {
   raw_csv_from_github = read.csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv')
   write.csv(raw_csv_from_github, 'data/global-latest-data.csv')
 }
-
 covid_data = read.csv('data/global-latest-data.csv')
 
 # Enrich data with new informations
@@ -12,13 +12,13 @@ covid_data = shift.column(data=covid_data, columns="tamponi", newNames='nuovi_ta
 covid_data['nuovi_tamponi'] = covid_data['tamponi'] - covid_data['nuovi_tamponi']
 covid_data['positivi/tamponi'] = covid_data['nuovi_positivi'] / covid_data['nuovi_tamponi']
 
+# Save new dataset
 write.csv(covid_data, 'data/global-latest-data-with-proportions.csv')
 
-# Save new dataset
-
+# Parse date column
 dates = as.Date(covid_data['data'][,1])
 
 plot(data.frame(dates, covid_data['positivi/tamponi'][,1]), '', 'positives / tests', type='l', col='red')
-plot(data.frame(dates, covid_data['totale_positivi'][,1]), '', 'total positives', type='l', col='yellow')
+plot(data.frame(dates, covid_data['totale_positivi'][,1]), '', 'total positives', type='l', col='purple')
 plot(data.frame(dates, covid_data['nuovi_positivi'][,1]), '', 'daily positives', type='l', col='black')
 plot(data.frame(dates, covid_data['tamponi']), '', 'tests', type='l', col='black')
