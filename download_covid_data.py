f61c406223ffe2bcb881d413c1da2d153171808a
Luca Terrazzan <luca.terraz@gmail.com>
2021-02-19 12:45:04 +0100

    fixed python plotting and added npm

--- download_covid_data.py
+++ download_covid_data.py
@@ -15,7 +15,6 @@ def main(download):
         print('Downloading fresh data from github...')
         pd.read_csv(GITHUB_CSV_URL, index_col='data').to_csv(LOCAL_CSV_PATH)
         print('...Done!')
-    quit()
     covid_data = pd.read_csv(LOCAL_CSV_PATH)
     covid_data['data'] = pd.to_datetime(covid_data['data'])
     covid_data.set_index('data', inplace=True)
@@ -28,13 +27,19 @@ def main(download):
     covid_data.to_csv(LOCAL_ANALYSIS_CSV_PATH)
 
     # Plot
-    fix, axs = plt.subplots(4)
+    fix, axs = plt.subplots(5, figsize=(10,20))
     axs[0].plot(covid_data['positivi/tamponi'])
+    axs[0].set_title('positives / tests')
     axs[1].plot(covid_data['totale_positivi'])
+    axs[1].set_title('total positives')
     axs[2].plot(covid_data['nuovi_positivi'])
+    axs[2].set_title('daily positives')
     axs[3].plot(covid_data['tamponi'])
+    axs[3].set_title('tests')
+    axs[4].plot(covid_data['deceduti'])
+    axs[4].set_title('deaths')
     # plt.show()
-    plt.savefig('fig.pdf')
+    plt.savefig('covid_data.pdf')
 
 
 if __name__ == "__main__":
